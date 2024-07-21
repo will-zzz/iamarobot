d999bd17cd3c437a100f2022c403d46c1922b6fe
Will Zakielarz <wmz1st@gmail.com>
2024-07-21 19:47:02 -0400

    tsconfig

--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,33 @@
-const { OpenAI } = require("openai");
-require("dotenv").config();
+import OpenAI from "openai";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [
-      { role: "system", content: "You are a helpful assistant." },
-      { role: "user", content: "What is a LLM?" },
-    ],
-    model: "gpt-4o-mini",
-  });
+const app = express();
+const port = process.env.PORT;
+app.use(express.json());
 
-  console.log(completion.choices[0]);
-}
-main();
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.post("/chat", async (req: Request, res: Response) => {
+  try {
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: req.body.message },
+      ],
+      model: "gpt-4o-mini",
+    });
+    res.send(completion.choices[0].message.content);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
